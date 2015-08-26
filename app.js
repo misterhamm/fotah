b823bcf88449f632def50e01fbcfe411223ba382
Chris Hamm <chris.m.hamm@gmail.com>
2015-08-25 23:11:20 -0400

    basic call and search functionality working, working on styling

--- app.js
+++ app.js
@@ -2,22 +2,74 @@ angular.module('fotah', ['ngMessages', 'ngAnimate'])
     
     .config(function($httpProvider) {
         $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
     })
     
     .controller('fotahCtrl', function($scope, $http, $sce) {
-        $http.get('https://api.instagram.com/v1/tags/kids/media/recent?client_id=69d05db321f94129af87c8b5f2eb614a&count=15');
-        console.log(data.
+        $scope.trustSrc = function(src) {
+            return $sce.trustAsResourceUrl(src);
+        };    
+    
+        $scope.searchInstagram = function(keyword, url) {
+            
+            if (url == undefined ) {
+                var searchUrl = "https://api.instagram.com/v1/tags/" + keyword + "/media/recent";
+            }
+            
+            else {
+                var searchUrl = $scope.moreResults
+            }
+            
+            var request = {
+                client_id: "69d05db321f94129af87c8b5f2eb614a",
+                count: 15,
+                callback: 'JSON_CALLBACK'
+            };
+            
+            $http({
+                method: 'JSONP',
+                url: searchUrl,
+                params: request
+            })
+            
+            .then(function(results) {
+                $scope.photos = results.data.data;
+                $scope.moreResults = results.data.pagination.next_url;
+            },
+            
+            function(results) {
+                alert('error');
+            });
+            
+            $scope.keyword = "";
+        }
+        
+        $scope.keyInput = function(enterKey, keyword) {
+            if (enterKey.keyCode != 13) {
+                return false;
+            }
+            else {
+                $scope.searchInstagram(keyword);
+            }
+                
+        }
+        
+            
+        
 });
     
     
+     /*$http.get('https://api.instagram.com/v1/tags/kids/media/recent?client_id=69d05db321f94129af87c8b5f2eb614a&count=15')
+        .then(function(data){
+                console.log(data)
+        })*/
     
     
     
     
     
     
-    
-       /* $scope.embedUrl = "https://api.instagram.com/v1/tags/kids/media/recent?client_id=69d05db321f94129af87c8b5f2eb614a&count=15";
+        /*$scope.embedUrl = "https://api.instagram.com/v1/tags/kids/media/recent?client_id=69d05db321f94129af87c8b5f2eb614a&count=15";
         
         $scope.trustSrc = function(src) {
             return $sce.trustAsResourceUrl(src);
@@ -27,9 +79,9 @@ angular.module('fotah', ['ngMessages', 'ngAnimate'])
             
             $scope.keyword = keyword;
             
-            var url = "https://api.instagram.com/vi/tags/";
+            var url = "https://api.instagram.com/vi/tags/" + keyword + "/media/recent/";
+            console.log(url);
             var request = {
-                q: keyword,
                 client_id: "69d05db321f94129af87c8b5f2eb614a",
                 count: 15
             };
@@ -41,16 +93,16 @@ angular.module('fotah', ['ngMessages', 'ngAnimate'])
             })
             
                 
-});
+});*/
 
 
-
+/*
 
 	$scope.searchInstagram = function(keyword) {
 			
   	$scope.keyword = keyword;
 
-	  var url = "http://www.youtube.com/embed/";
+	  var url = "http://www.youtube.com/embed/" ;
 	  var request = {
 		  key: "AIzaSyBIT2homLIII4fhIFO1ePwQDKA_rm2Ym30",
 		  part: "snippet",
